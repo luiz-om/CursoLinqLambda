89a8e5e170137a10dc5dae5949046107b470e211
Luiz-OM <luizfernando_martins@hotmail.com>
2019-07-09 11:49:30 -0400

    aula3

---
+++ Loja.Domain/Loja.Domain/Produto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loja.Domain
+{
+   public class Produto
+    {
+        private List<Produto> produtos;
+
+        public Produto()
+        {
+            produtos = new List<Produto>();
+        }
+
+        public List<Produto> Listar()
+        {
+            produtos.Add(new Produto { ProdutoId=Guid.NewGuid(), Nome ="Maracuja", Quantidade= 12, Valor= 14, DataVencimento = DateTime.Now.AddDays(15)});
+            produtos.Add(new Produto { ProdutoId=Guid.NewGuid(), Nome ="Banana", Quantidade= 112, Valor= 2, DataVencimento = DateTime.Now.AddDays(12)});
+            produtos.Add(new Produto { ProdutoId=Guid.NewGuid(), Nome ="Maça", Quantidade= 2, Valor= 25, DataVencimento = DateTime.Now.AddDays(15)});
+            produtos.Add(new Produto { ProdutoId=Guid.NewGuid(), Nome ="Uva", Quantidade= 115, Valor= 4, DataVencimento = DateTime.Now.AddDays(7)});
+            return produtos;
+        }
+        public Guid ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public int Quantidade { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataVencimento { get; set; }
+
+    }
+}
--- Loja/Program.cs
+++ Loja/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Loja.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +13,21 @@ namespace Loja
         {
 
 
+            //var produto = new Produto();
 
+            //var pro = produto.Listar().Where(p => p.Valor>4);
+
+            //foreach (var item in pro)
+            //{
+            //    Console.WriteLine(item.Nome);
+            //}
+
+            new Produto().Listar().Where(p => p.Valor>4 ).ToList().ForEach(item => {
+                Console.WriteLine(item.Nome);
+                Console.WriteLine(item.ProdutoId.ToString());
+
+            });
+            Console.ReadKey();
 
         }
     }
